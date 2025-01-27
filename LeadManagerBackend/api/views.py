1a38ae6cad2634b38c5b4ce04cefdfa0cf50c2d9
Rebin <muhammedrebin1794@gmail.com>
2025-01-27 17:49:30 +0530

    Updated lead list view for search filtering

--- LeadManagerBackend/api/views.py
+++ LeadManagerBackend/api/views.py
@@ -8,13 +8,24 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView,ListAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView
 from api.serializers import LeadSerializer
 from api.models import Lead
+from django.db.models import Q
 
 class LeadListCreateView(CreateAPIView,ListAPIView):
 
     serializer_class=LeadSerializer
 
     queryset=Lead.objects.all()
-
+    
+    def get_queryset(self):
+        filter_text = self.request.query_params # {'search_text':'hello'}
+        
+        qs = Lead.objects.all()
+        
+        if filter_text:
+            search_value = filter_text.get('search_text')
+            qs = qs.filter(Q(name__contains=search_value)|Q(email__contains=search_value))
+            
+        return qs
 
 
 class LeadRetrieveUpdateDestroyView(RetrieveAPIView,UpdateAPIView,DestroyAPIView):
